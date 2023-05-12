93286d908fad5839d01f650beefb69f6d9a732c3
Sanket <sanket.thakre@infinitylearn.com>
2023-05-13 01:29:48 +0530

    Voice recognition, bootstrap and video transitions added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { VoiceRecognitionService } from './service/voice-recognition.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Teaching-Karaoke';
+  text: string = '';
+  voiceRecognition: boolean = false;
+  videoEnd : boolean = false;
+
+  constructor( public service : VoiceRecognitionService){
+    this.service.init()
+  }
+
+  ngOnInit(): void {
+  }
+
+  startService(){
+    this.voiceRecognition = true;
+    this.service.start()
+  }
+
+  stopService(){
+    this.voiceRecognition = false;
+    this.service.stop()
+  }
+
+  onVideoEnded(){
+    alert('Video is ended')
+    this.videoEnd = true;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {VgCoreModule} from '@videogular/ngx-videogular/core';
+import {VgControlsModule} from '@videogular/ngx-videogular/controls'
+import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play'
+import {VgBufferingModule} from '@videogular/ngx-videogular/buffering'
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    VgBufferingModule,
+    VgOverlayPlayModule,
+    VgControlsModule,
+    VgCoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
